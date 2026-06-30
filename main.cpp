bef98eee636900d36447c665b51cd3d5f441151d
DAVI BOLSONI <davi_bolsoni@estudante.sesisenai.org.br>
2026-06-30 17:06:59 -0300

    luta adicionada

---
+++ main.cpp
@@ -0,0 +1,47 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main () {
+	int ataque, vida, danoFinal
+	string Tigrinho = 3, Ademar = 2, Robson = 1;
+	
+	cout << "Seja bem vindo a Masmorra";
+	cout << "Voce jogarar com Robson obson";
+	cout << "==================================";
+	cout << "===Robson obson.     1         ===";
+	cout << "===|                           ===";
+	cout << "===|->Vida:120                 ===";
+	cout << "===|->Ataque:30                ===";
+	cout << "===|->mana:35                  ===";
+	cout << "===|->Ataque especial          ===";
+	cout << "===|     |->Sedução perfeita   ===";
+	cout << "==================================";
+	
+	cout << "\n esses seram seus inimigos\n";
+	
+	cout << "===Ademar o sábio.   2         ===";
+	cout << "===|                           ===";
+	cout << "===|->Vida:80                  ===";
+	cout << "===|->Ataque:20                ===";
+	cout << "===|->mana:50                  ===";
+	cout << "===|->Ataque especial          ===";
+	cout << "===|     |->Meteoro            ===";
+	cout << "==================================";
+	cout << "===Tigrinho pançudo. 3         ===";
+	cout << "===|                           ===";
+	cout << "===|->Vida:50                  ===";
+	cout << "===|->Ataque:25                ===";
+	cout << "===|->mana:25                  ===";
+	cout << "===|->Ataque especial          ===";
+	cout << "===|     |->golpe na cara dura ===";
+	
+	cout << "Ademar o sabio esta na sua frete oque vc faz?";
+	cout << "--> Ataque";
+	cout << "--> defesa";
+	cin << ataque << defesa;
+	cout << "Ademar usarar... ataque! "
+	
+	
+}
