ee460d6d40d1dd26e6691f7ba4b201e474f53d56
HELLORÁ BUENO MIRANDA <hellora_miranda@estudante.sesisenai.org.br>
2026-06-30 16:35:01 -0300

    combate.h adicionado com funções show, damage e isitalive.

---
+++ combate.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int show {
+
+string nome;
+int vida;
+int ataque;
+
+cout << "nome do personagem: " << nome << endl << "vida: " << vida << endl << "nivel do ataque: " << ataque << endl;
+
+}
+
+int damage {
+
+int vidaAtual;
+int ataque;
+int defesa;
+
+vidaAtual = (ataque - defesa) - vida
+
+}
+
+int isitalive {
+
+if (vidaAtual >= 0) {
+	cout << "você está vivo";
+}else {
+	cout << "você está morto";
+	
+}
+}
+
+return 0;
